70810d5b6231d598cece38e50bc4bb8e4185b05c
Ozan ERTÜRK <ozanerturk17@gmail.com>
2020-12-24 15:59:04 +0300

    tr version of vibration calculator updated

--- docs/iframes/vibration_calculator/main.js
+++ docs/iframes/vibration_calculator/main.js
@@ -12,40 +12,40 @@ define(function (require) {
     var exampleData = require('./signaldata');
     const reader = new FileReader();
 
-    loader.style="display:none";
-    resultsDom.style="display:none";
+    loader.style = "display:none";
+    resultsDom.style = "display:none";
 
-    downloadSample.addEventListener("click",()=>{
-            var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exampleData));
-            var downloadAnchorNode = document.createElement('a');
-            downloadAnchorNode.setAttribute("href",     dataStr);
-            downloadAnchorNode.setAttribute("download", "6000Hz_100Hz" + ".json");
-            document.body.appendChild(downloadAnchorNode); // required for firefox
-            downloadAnchorNode.click();
-            downloadAnchorNode.remove();
+    downloadSample.addEventListener("click", () => {
+        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exampleData));
+        var downloadAnchorNode = document.createElement('a');
+        downloadAnchorNode.setAttribute("href", dataStr);
+        downloadAnchorNode.setAttribute("download", "6000Hz_100Hz" + ".json");
+        document.body.appendChild(downloadAnchorNode); // required for firefox
+        downloadAnchorNode.click();
+        downloadAnchorNode.remove();
     })
 
     file.addEventListener("change", (event) => {
         const fileList = event.target.files;
         reader.readAsDataURL(fileList[0]);
-        loader.style="display:block";
-        resultsDom.style="display:none"
+        loader.style = "display:block";
+        resultsDom.style = "display:none"
     })
     reader.addEventListener('load', (event) => {
-    
+
         const data = JSON.parse(atob(event.target.result.split("data:application/json;base64,")[1]));
         let dataX = data.data.accelerometer_x
         let dataY = data.data.accelerometer_y
         let dataZ = data.data.accelerometer_z
         let calibratedSamplingRate = parseInt(data.calibratedSamplingRate)
-
+        results.innerHTML = ""
         putResult("Grms X", signalProcessor.Grms_score(dataX))
         putResult("Grms Y", signalProcessor.Grms_score(dataY))
         putResult("Grms Z", signalProcessor.Grms_score(dataZ))
 
-        putResult("Vrms X", signalProcessor.Vrms_score(dataX,calibratedSamplingRate))
-        putResult("Vrms Y", signalProcessor.Vrms_score(dataY,calibratedSamplingRate))
-        putResult("Vrms Z", signalProcessor.Vrms_score(dataZ,calibratedSamplingRate))
+        putResult("Vrms X", signalProcessor.Vrms_score(dataX, calibratedSamplingRate))
+        putResult("Vrms Y", signalProcessor.Vrms_score(dataY, calibratedSamplingRate))
+        putResult("Vrms Z", signalProcessor.Vrms_score(dataZ, calibratedSamplingRate))
 
         putResult("Clearance X", signalProcessor.Clearance(dataX))
         putResult("Clearance Y", signalProcessor.Clearance(dataY))
@@ -60,15 +60,15 @@ define(function (require) {
         putResult("Crest Y", signalProcessor.Crest(dataY))
         putResult("Crest Z", signalProcessor.Crest(dataZ))
 
-        putResult("Crest_star X", signalProcessor.Crest_star(dataX,calibratedSamplingRate))
-        putResult("Crest_star Y", signalProcessor.Crest_star(dataY,calibratedSamplingRate))
-        putResult("Crest_star Z", signalProcessor.Crest_star(dataZ,calibratedSamplingRate))
+        putResult("Crest_star X", signalProcessor.Crest_star(dataX, calibratedSamplingRate))
+        putResult("Crest_star Y", signalProcessor.Crest_star(dataY, calibratedSamplingRate))
+        putResult("Crest_star Z", signalProcessor.Crest_star(dataZ, calibratedSamplingRate))
 
         putResult("Kurtosis X", signalProcessor.Kurtosis(dataX))
         putResult("Kurtosis Y", signalProcessor.Kurtosis(dataY))
         putResult("Kurtosis Z", signalProcessor.Kurtosis(dataZ))
-        loader.style="display:none";
-        resultsDom.style="display:block";
+        loader.style = "display:none";
+        resultsDom.style = "display:block";
     })
     function putResult(label, result) {
         results.appendChild(createElementFromHTML(`<tr><td>${label}</td><td>${result}</td></tr>`));
@@ -82,7 +82,7 @@ define(function (require) {
         tbody.innerHTML = htmlString.trim();
 
         // Change this to div.childNodes to support multiple top-level nodes
-        console.log( tbody.firstChild)
+        console.log(tbody.firstChild)
         return tbody.firstChild;
     }
 
